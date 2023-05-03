d4df338d0ed0823b8806c2483dba19b6d7576ada
7ngenious <zxvs1218@gmail.com>
2023-05-03 20:23:07 +0900

    test: JWT login & Recommended API

--- src/main/java/com/nimatnemat/nine/domain/recommended/RecommendedController.java
+++ src/main/java/com/nimatnemat/nine/domain/recommended/RecommendedController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +32,10 @@ public class RecommendedController {
             return new ResponseEntity<>(null, HttpStatus.OK);
         }
     }
-    @GetMapping("/{userId}/second")
+    @GetMapping("/second")
     @Operation(summary = "사용자별 두 번째 추천 레스토랑 조회 API", description = "사용자 ID에 따라 두 번째 추천 레스토랑 목록을 반환합니다.")
-    public ResponseEntity<List<String>> getSecondRecommendationsByUserId(@PathVariable String userId) {
-        Optional<List<Recommended>> recommendations = recommendedService.findByUserId(userId);
+    public ResponseEntity<List<String>> getSecondRecommendationsByUserId(Authentication authentication) {
+        Optional<List<Recommended>> recommendations = recommendedService.findByUserId(authentication.getName());
 
         if (recommendations.isPresent() && !recommendations.get().isEmpty()) {
             List<String> secondRecommend = recommendations.get().get(0).getSecondRecommend();
